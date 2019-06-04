2d4f993eca97820d74714ca64705f63ba7f000a4
Vitsniper <50515949+Vitsniper@users.noreply.github.com>
2019-06-04 17:11:20 +0300

    Added methods for cookies

--- Wordpress/Tools/BrowserManager.py
+++ Wordpress/Tools/BrowserManager.py
@@ -1,6 +1,10 @@
+import pickle
+import pprint as pp
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+
 
 class Browser:
 
@@ -11,30 +15,32 @@ class Browser:
     OPERA = 5
 
     browser_instance = None
+    cookies_location = "C:\\Users\\Vitalik\\PycharmProjects\\Selenium\\" \
+                       "venv\\PageObjectModel\\Wordpress\\Tools\\cookies.txt"
 
     @staticmethod
     def create_new_driver(driver_id):
         def set_driver():
             if Browser.PHANTOM == driver_id:
-                driver = webdriver.PhantomJS()
+                Browser.browser_instance = webdriver.PhantomJS()
 
             elif Browser.CHROME == driver_id:
                 chrome_options = Options()
                 chrome_options.add_argument("--headless")
-                # driver = webdriver.Chrome(options=chrome_options)
-                driver = webdriver.Chrome()
+                # Browser.browser_instance = webdriver.Chrome(options=chrome_options)
+                Browser.browser_instance = webdriver.Chrome()
 
             elif Browser.FF == driver_id:
-                driver = webdriver.Firefox()
+                Browser.browser_instance = webdriver.Firefox()
 
             elif Browser.OPERA == driver_id:
-                driver = webdriver.Opera()
+                Browser.browser_instance = webdriver.Opera()
 
             elif Browser.IE == driver_id:
-                driver = webdriver.Ie()
+                Browser.browser_instance = webdriver.Ie()
             else:
                 raise Exception("There is no support for driver_id: {}".format(driver_id))
-            return driver
+            return Browser.browser_instance
 
         Browser.browser_instance = set_driver()
         return Browser.browser_instance
@@ -91,4 +97,33 @@ class Browser:
     def get_alert_text():
         Browser.browser_instance.switch_to_alert.text
 
+    @staticmethod
+    def save_cookies(cookies_location = Browser.cookies_location):
+        pickle.dump(Browser.browser_instance.get_cookies(), open(cookies_location, "wb"))
+
+    @staticmethod
+    def load_cookies(cookies_location = Browser.cookies_location, url=None):
+        cookies = pickle.load(open(cookies_location, "rb"))
+        Browser.browser_instance.delete_all_cookies()
+        # have to be on a page before you can add any cookies, any page - does not matter which
+        Browser.browser_instance.get("https://google.com" if url is None else url)
+        for cookie in cookies:
+            Browser.browser_instance.add_cookie(cookie)
+
+    @staticmethod
+    def delete_cookies(domains=None):
+
+        if domains is not None:
+            cookies = Browser.browser_instance.get_cookies()
+            original_len = len(cookies)
+            for cookie in cookies:
+                if str(cookie["domain"]) in domains:
+                    cookies.remove(cookie)
+            if len(cookies) < original_len:  # if cookies changed, we will update them
+                # deleting everything and adding the modified cookie object
+                Browser.browser_instance.delete_all_cookies()
+                for cookie in cookies:
+                    Browser.browser_instance.add_cookie(cookie)
+        else:
+            Browser.browser_instance.delete_all_cookies()
     #TODO: is_alert_displayed
